52f1a6f188cc48e257d3124e5dc08f513de5f9f3
Manuel Caroca <manuel@byteservices.cl>
2020-01-22 09:58:32 -0300

    Cambio de logica en el validador de nombre

--- TestDesarrolloFront/src/app/core/nombre.validator.ts
+++ TestDesarrolloFront/src/app/core/nombre.validator.ts
@@ -4,7 +4,7 @@ export function ValidateNombre(control: AbstractControl) {
 
     let nombre = control.value.toString().split(' ');
 
-    if(nombre.length <= 3) {
+    if(nombre.filter(f => f !== '').length <= 2) {
         return {
             nombre: true
         }
